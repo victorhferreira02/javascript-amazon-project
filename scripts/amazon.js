5eefece64409cf5f2ded808186a95768bf95ca38
Victorhf <victorhferreira02@gmail.com>
2025-12-01 08:20:43 -0300

    Making the quantity "selector" useful

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -18,7 +18,7 @@ products.forEach((product) => {
             <div class="product-price">${(product.priceCents / 100).toFixed(2)}</div>
 
             <div class="product-quantity-container">
-              <select>
+              <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -61,12 +61,16 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+    const quantity = Number(quantitySelector.value);
+
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: 1,
+        productId,
+        quantity,
       });
     }
 
