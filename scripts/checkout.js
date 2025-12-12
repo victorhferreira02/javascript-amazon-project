b37c3dd94351b20986db0a2ab33aa8be6a43be79
Victorhf <victorhferreira02@gmail.com>
2025-12-12 10:15:15 -0300

    Making cart delete buttons useful

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -28,7 +28,7 @@ cart.forEach((cartItem) => {
           <div class="product-quantity">
             <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
             <span class="update-quantity-link link-primary"> Update </span>
-            <span class="delete-quantity-link link-primary"> Delete </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id='${matchingProduct.id}'> Delete </span>
           </div>
         </div>
 
@@ -62,3 +62,10 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+  });
+});
