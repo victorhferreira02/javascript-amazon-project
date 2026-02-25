3da5423c8445a1ea5366bf1e8b763b564902bd20
Victorhf <victorhferreira02@gmail.com>
2026-02-25 09:55:16 -0300

    Save the Data and generate HTML for paymen summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./utils/checkout/orderSummary.js";
+import { renderPaymentSummary } from "./utils/checkout/paymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
